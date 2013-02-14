722abb970b0bf6ed790f492798c3560ec872e478
HyungKyu Song <hk76.song@samsung.com>
2013-02-14 21:43:10 +0900

    Tizen 2.0 Release

---
+++ include/favorites_private.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2012 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __TIZEN_WEB_FAVORITES_PRIVATE_H__
+#define __TIZEN_WEB_FAVORITES_PRIVATE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**********************************************
+ * Logging macro
+ **********************************************/
+#define FAVORITES_LOGD(fmt, args...) LOGD(\
+		"[%s: %s: %d] "fmt, (rindex(__FILE__, '/')? rindex(__FILE__, '/') + 1 : __FILE__),\
+		__FUNCTION__, __LINE__, ##args)
+#define FAVORITES_LOGI(fmt, args...) LOGI(\
+		"[%s: %s: %d] "fmt, (rindex(__FILE__, '/') ? rindex(__FILE__, '/') + 1 : __FILE__),\
+		__FUNCTION__, __LINE__, ##args)
+#define FAVORITES_LOGW(fmt, args...) LOGW(\
+		"[%s: %s: %d] "fmt, (rindex(__FILE__, '/') ? rindex(__FILE__, '/') + 1 : __FILE__),\
+		__FUNCTION__, __LINE__, ##args)
+#define FAVORITES_LOGE(fmt, args...) LOGE(\
+		"[%s: %s: %d] "fmt, (rindex(__FILE__, '/') ? rindex(__FILE__, '/') + 1 : __FILE__),\
+		__FUNCTION__, __LINE__, ##args)
+#define FAVORITES_LOGE_IF(cond, fmt, args...) LOGE_IF(cond,\
+		"[%s: %s: %d] "fmt, (rindex(__FILE__, '/') ? rindex(__FILE__, '/') + 1 : __FILE__),\
+		__FUNCTION__, __LINE__, ##args)
+
+/**********************************************
+ * Argument checking macro
+ **********************************************/
+#define FAVORITES_NULL_ARG_CHECK(_arg_)	do { \
+	if(_arg_ == NULL) { \
+        LOGE("[%s] FAVORITES_ERR_INVALID_PARAMETER(0x%08x)", __FUNCTION__,\
+        FAVORITES_ERROR_INVALID_PARAMETER); \
+        return FAVORITES_ERROR_INVALID_PARAMETER; \
+    } \
+}while(0)
+
+#define FAVORITES_INVALID_ARG_CHECK(_condition_)	do { \
+	if(_condition_) { \
+        LOGE("[%s] FAVORITES_ERR_INVALID_PARAMETER(0x%08x)", __FUNCTION__,\
+        FAVORITES_ERROR_INVALID_PARAMETER); \
+        return FAVORITES_ERROR_INVALID_PARAMETER; \
+    } \
+}while(0)
+
+#define _FAVORITES_FREE(_srcx_) 	{	if(NULL != _srcx_) free(_srcx_);	}
+#define _FAVORITES_STRDUP(_srcx_) 	(NULL != _srcx_) ? strdup(_srcx_):NULL
+
+struct bookmark_entry_internal{
+	char* address;		/**< URL of the bookmark */
+	char* title;			/**< Title of the bookmark */
+	char* creationdate;	/**< date of created */
+	char* updatedate;	/**< date of last updated */
+	char* visitdate;		/**< date of last visited */
+	int id;				/**< uniq id of bookmark */
+	int is_folder;			/**< property bookmark or folder 1: bookmark 1: folder */
+	int folder_id;			/**< parent folder id */
+	int orderIndex;		/**< order sequence */
+	int editable;			/**< flag of editability 1 : WRITABLE   0: READ ONLY */
+};
+typedef struct bookmark_entry_internal bookmark_entry_internal_s;
+typedef struct bookmark_entry_internal *bookmark_entry_internal_h;
+
+struct bookmark_list {
+	int count;
+	bookmark_entry_internal_h item;
+};
+
+/**
+ * @brief The bookmark entry list handle.
+ */
+typedef struct bookmark_list bookmark_list_s;
+
+/**
+ * @brief The bookmark entry list structure.
+ */
+typedef struct bookmark_list *bookmark_list_h;
+
+struct favicon_entry {
+	void *data;	/* favicon image data pointer. ( Allocated memory) */
+	int length;	/* favicon image data's length */
+	int w;		/* favicon image width */
+	int h;		/* favicon image height */
+};
+typedef struct favicon_entry favicon_entry_s;
+typedef struct favicon_entry *favicon_entry_h;
+
+/* bookmark internal API */
+void _favorites_close_bookmark_db(void);
+void _favorites_finalize_bookmark_db(sqlite3_stmt *stmt);
+const char *_favorites_get_bookmark_db_name(void);
+int _favorites_open_bookmark_db(void);
+void _favorites_free_bookmark_list(bookmark_list_h m_list);
+int _favorites_free_bookmark_entry(favorites_bookmark_entry_s *entry);
+int _favorites_get_bookmark_lastindex(int locationId);
+int _favorites_bookmark_get_folderID(const char *foldername);
+int _favorites_bookmark_is_folder_exist(const char *foldername);
+int _favorites_bookmark_create_folder(const char *foldername);
+int _favorites_bookmark_is_bookmark_exist(const char *url, const char *title, const int folderId);
+int _favorites_get_bookmark_count_at_folder(int folderId);
+int _favorites_bookmark_get_folder_count(void);
+bookmark_list_h _favorites_get_bookmark_list_at_folder(int folderId);
+bookmark_list_h _favorites_bookmark_get_folder_list(void);
+int _favorites_get_unixtime_from_datetime(char *datetime);
+int _favorites_bookmark_get_bookmark_id(const char *url, const char *title, const int folder_id);
+
+/* history internal API */
+void _favorites_history_db_close(void);
+void _favorites_history_db_finalize(sqlite3_stmt *stmt);
+int _favorites_history_db_open(void);
+int _favorites_free_history_entry(favorites_history_entry_s *entry);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* __TIZEN_WEB_FAVORITES_PRIVATE_H__ */
---
+++ packaging/capi-web-favorites.spec
@@ -0,0 +1,92 @@
+Name:       capi-web-favorites
+Summary:    Internet bookmark and history API
+Version:    0.0.7
+Release:    1
+Group:      TO_BE/FILLED_IN
+License:    Apache License, Version 2.0
+Source0:    %{name}-%{version}.tar.gz
+BuildRequires:  cmake
+BuildRequires:  pkgconfig(dlog)
+BuildRequires:  pkgconfig(db-util)
+BuildRequires:  pkgconfig(evas)
+BuildRequires:  pkgconfig(capi-base-common)
+Requires(post): /sbin/ldconfig  
+Requires(postun): /sbin/ldconfig
+
+%description
+
+
+%package devel
+Summary:  Internet Bookmark and History in Tizen Native API (Development)
+Group:    TO_BE/FILLED_IN
+Requires: %{name} = %{version}-%{release}
+
+%description devel
+
+
+
+%prep
+%setup -q
+
+
+%build
+%cmake .
+make %{?jobs:-j%jobs}
+
+%install
+%make_install
+
+%post
+/sbin/ldconfig
+mkdir -p /opt/usr/dbspace/
+##### History ######
+if [ ! -f /opt/usr/dbspace/.browser-history.db ];
+then
+	sqlite3 /opt/usr/dbspace/.browser-history.db 'PRAGMA journal_mode=PERSIST;
+	CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT, address, title, counter INTEGER, visitdate DATETIME, favicon BLOB, favicon_length INTEGER, favicon_w INTEGER, favicon_h INTEGER);'
+fi
+
+### Bookmark ###
+if [ ! -f /opt/usr/dbspace/.internet_bookmark.db ];
+then
+	sqlite3 /opt/usr/dbspace/.internet_bookmark.db 'PRAGMA journal_mode=PERSIST;
+	CREATE TABLE bookmarks(id INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, parent INTEGER, address, title, creationdate, sequence INTEGER, updatedate, visitdate, editable INTEGER, accesscount INTEGER, favicon BLOB, favicon_length INTEGER, favicon_w INTEGER, favicon_h INTEGER);
+	create index idx_bookmarks_on_parent_type on bookmarks(parent, type);
+
+	insert into bookmarks (type, parent, title, creationdate, editable, sequence, accesscount) values(1, 0, "Bookmarks", DATETIME("now"),  0, 1, 0);'
+fi
+
+# Change db file owner & permission
+chown :5000 /opt/usr/dbspace/.browser-history.db
+chown :5000 /opt/usr/dbspace/.browser-history.db-journal
+chown :5000 /opt/usr/dbspace/.internet_bookmark.db
+chown :5000 /opt/usr/dbspace/.internet_bookmark.db-journal
+chmod 666 /opt/usr/dbspace/.browser-history.db
+chmod 666 /opt/usr/dbspace/.browser-history.db-journal
+chmod 666 /opt/usr/dbspace/.internet_bookmark.db
+chmod 666 /opt/usr/dbspace/.internet_bookmark.db-journal
+
+if [ -f /usr/lib/rpm-plugins/msm.so ]
+then
+	chsmack -a 'org.tizen.browser::db_external' /opt/usr/dbspace/.browser-history.db*
+	chsmack -a 'org.tizen.browser::db_external' /opt/usr/dbspace/.internet_bookmark.db*
+fi
+##################################################
+# set default vconf values
+##################################################
+#internal keys
+vconftool set -t string db/browser/browser_user_agent "System user agent" -g 5000 -f
+vconftool set -t string db/browser/custom_user_agent "" -g 5000 -f
+#public keys
+vconftool set -t string db/browser/user_agent "Mozilla/5.0 (Linux; U; Tizen 2.0; en-us) AppleWebKit/537.1 (KHTML, like Gecko) Version/2.0 Mobile" -g 5000 -f
+
+%postun -p /sbin/ldconfig
+
+
+%files
+%manifest capi-web-favorites.manifest
+%{_libdir}/libcapi-web-favorites.so
+
+%files devel
+%{_includedir}/web/*.h
+%{_libdir}/pkgconfig/*.pc
